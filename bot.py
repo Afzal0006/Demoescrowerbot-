d1bdaa5e97ee1fbc428589ceb3059f49e374e35f
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-09-01 19:48:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ MONGO_URI = "mongodb+srv://TRUSTLYTRANSACTIONBOT:TRUSTLYTRANSACTIONBOT@cluster0.
 LOG_CHANNEL_ID = -1002826823679
 
 # Multiple owner IDs
-OWNER_IDS = [7727059592]  # Add as many IDs as you want
+OWNER_IDS = [7363327309]  # Add as many IDs as you want
 
 # ==== MONGO CONNECT ====
 client = MongoClient(MONGO_URI)
@@ -240,6 +240,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
